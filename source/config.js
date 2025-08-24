2f332b40cc333394b82017ed901e5e4e6f04aa16
shenyouchu <zc15108962790@163.com>
2025-08-25 01:06:04 +0800

    主页面和二级页面增加备案信息

---
+++ source/config.js
@@ -0,0 +1,12 @@
+// 备案信息配置文件
+const siteConfig = {
+    // 备案信息
+    beian: {
+        icp: "琼ICP备    号", // ICP备案号
+        police: "琼公网安备       号", // 公安备案号
+        info: "海口希灵赛斯网络科技有限责任公司" // 公司信息
+    }
+};
+
+// 使配置在全局可访问
+window.siteConfig = siteConfig;
