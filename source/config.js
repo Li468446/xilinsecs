c7f65452c14ae71d49902faf32add8cf2c899805
shenyouchu <zc15108962790@163.com>
2025-08-28 13:10:08 +0800

    更新ICP备案信息

--- source/config.js
+++ source/config.js
@@ -2,7 +2,7 @@
 const siteConfig = {
     // 备案信息
     beian: {
-        icp: "琼ICP备    号", // ICP备案号
+        icp: "琼ICP备2025060601号", // ICP备案号
         police: "琼公网安备       号", // 公安备案号
         info: "海口希灵赛斯网络科技有限责任公司" // 公司信息
     }
