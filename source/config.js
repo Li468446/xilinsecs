49a4ce392752458f8f8f1e9b84220dac80e829d0
shenyouchu <zc15108962790@163.com>
2025-08-31 15:58:33 +0800

    去除公安备案信息

--- source/config.js
+++ source/config.js
@@ -3,7 +3,7 @@ const siteConfig = {
     // 备案信息
     beian: {
         icp: "琼ICP备2025060601号", // ICP备案号
-        police: "琼公网安备       号", // 公安备案号
+        police: "                 ", // 公安备案号
         info: "海口希灵赛斯网络科技有限责任公司" // 公司信息
     }
 };
