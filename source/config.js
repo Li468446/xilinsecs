ed7237bbc9744437b1c3d874726c18894d4d3413
shenyouchu <zc15108962790@163.com>
2025-09-05 15:22:32 +0800

    修正页脚显示链接关系

--- source/config.js
+++ source/config.js
@@ -2,11 +2,11 @@
 const siteConfig = {
     // 备案信息
     beian: {
-        icp: "琼ICP备2025060601号-1", // ICP备案号
-        police: "                 ", // 公安备案号
+        icp: "琼ICP备2025060601号", // ICP备案号
+        police: "                   ", // 公安备案号
         info: "海口希灵赛斯网络科技有限责任公司" // 公司信息
     }
 };
 
 // 使配置在全局可访问
-window.siteConfig = siteConfig; 
+window.siteConfig = siteConfig;
